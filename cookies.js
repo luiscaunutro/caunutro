cd97adefd9a2e7d8b86c993a7beb30dcb5e2e138
luiscaunutro <168844651+luiscaunutro@users.noreply.github.com>
2024-06-25 17:16:03 -0300

    cookies.js

---
+++ cookies.js
@@ -0,0 +1,18 @@
+// Adiciona o script do Cookie Consent ao documento
+(function() {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://cookieconsent.popupsmart.com/src/js/popper.js';
+    script.onload = function() {
+        window.start.init({
+            Palette: "palette2",
+            Mode: "floating left",
+            Theme: "wire",
+            Message: "Este site usa cookies para te entregar a melhor experiência.",
+            ButtonText: "Entendi",
+            LinkText: "Clique para maiores informações",
+            Time: "10",
+        });
+    };
+    document.head.appendChild(script);
+})();
